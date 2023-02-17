26c790708bf3caedb76a984170b0a769229671ee
Xinhe <aspasia.yu@gmail.com>
2023-02-17 18:24:12 +0100

    opt cite modal

---
+++ js/main.js
@@ -0,0 +1,77 @@
+/* Welcome Text in console */
+welcomeText = `Welcome to the personal website of
+                                    
+  ▄████████  ███    █▄    ▄█  
+ ███    ███  ███    ███  ███  
+ ███    ███  ███    ███  ███▌ 
+▄███▄▄▄▄██▀  ███    ███  ███▌ 
+▀▀███▀▀▀▀▀   ███    ███  ███▌ 
+▀███████████ ███    ███  ███  
+ ███    ███  ███    ███  ███  
+ ███    ███  ████████▀   █▀   
+ ███    ███                 
+
+https://rui-yuan91.github.io/
+`
+console.log(welcomeText);
+
+/* Reference variables */
+
+const snr_cite = ["snr_cite",
+    `@article{yuan2022sketched,
+    author = {Yuan, Rui and Lazaric, Alessandro and Gower, Robert M.},
+    title = {Sketched {N}ewton--{R}aphson},
+    journal = {SIAM Journal on Optimization},
+    volume = {32},
+    number = {3},
+    pages = {1555-1583},
+    year = {2022},
+}`
+];
+
+const san_cite = ["san_cite",
+    `@InProceedings{chen2022san,
+    title = { {SAN}: {S}tochastic Average {N}ewton Algorithm for Minimizing Finite Sums },
+    author = {Chen, Jiabin and Yuan, Rui and Garrigos, Guillaume and Gower, Robert M.},
+    booktitle = {Proceedings of The 25th International Conference on Artificial Intelligence and Statistics},
+    pages = {279--318},
+    year = {2022},
+    volume = {151},
+    series = {Proceedings of Machine Learning Research},
+    month = {28--30 Mar},
+    publisher = {PMLR},
+}`
+];
+
+const pg_cite = ["pg_cite",
+    `@InProceedings{yuan2022general,
+    title = { A general sample complexity analysis of vanilla policy gradient },
+    author = {Yuan, Rui and Gower, Robert M. and Lazaric, Alessandro},
+    booktitle = {Proceedings of The 25th International Conference on Artificial Intelligence and Statistics},
+    pages = {3332--3380},
+    year = {2022},
+    volume = {151},
+    series = {Proceedings of Machine Learning Research},
+    month = {28--30 Mar},
+    publisher = {PMLR},
+}`
+];
+
+const npg_cite = ["npg_cite",
+    `@inproceedings{yuan2023linear,
+    title = {Linear Convergence of Natural Policy Gradient Methods with Log-Linear Policies},
+    author = {Rui Yuan and Simon Shaolei Du and Robert M. Gower and Alessandro Lazaric and Lin Xiao},
+    booktitle = {International Conference on Learning Representations},
+    year = {2023},
+}`
+];
+
+const ampo_cite = ["ampo_cite",
+    `@misc{alfano2023novel,
+    doi = {10.48550/ARXIV.2301.13139},
+    author = {Alfano, Carlo and Yuan, Rui and Rebeschini, Patrick},
+    title = {A Novel Framework for Policy Mirror Descent with General Parametrization and Linear Convergence},
+    publisher = {arXiv},
+    year = {2023},
+}`
+];
